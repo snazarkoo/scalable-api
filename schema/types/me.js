108a3e67a84019b3081b4bae690be4450f4a7271
Nazar Shcherbiak <snazarkoo@gmail.com>
2018-01-07 22:57:10 +0200

    Added contests list schema

--- schema/types/me.js
+++ schema/types/me.js
@@ -2,14 +2,31 @@ const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
-  GraphQLNonNull
+  GraphQLNonNull,
+  GraphQLList
 } = require('graphql');
+
+const pgdb = require('../../database/pgdb');
+const ContestType = require('./contest');
 
 module.exports = new GraphQLObjectType({
   name: 'MeType',
 
   fields: {
     id: { type: GraphQLID },
-    email: { type: new GraphQLNonNull(GraphQLString) }
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    fullName: {
+      type: GraphQLString,
+      resolve: obj => `${obj.firstName} ${obj.lastName}`
+    },
+    email: { type: new GraphQLNonNull(GraphQLString) },
+    createdAt: { type: GraphQLString },
+    contests: {
+      type: new GraphQLList(ContestType),
+      resolve(obj, args, { pgPool }) {
+        return pgdb(pgPool).getContests(obj);
+      }
+    }
   }
 });
