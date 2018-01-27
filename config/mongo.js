531bf667a4043d574f4278ce6dc973588f85c6e8
Nazar Shcherbiak <snazarkoo@gmail.com>
2018-01-27 14:27:04 +0200

    Added node values

--- config/mongo.js
+++ config/mongo.js
@@ -1,5 +1,8 @@
 module.exports = {
   development: {
     url: 'mongodb://localhost:27017/contests'
+  },
+  production: {
+    url: process.env.NODE_MONGO_URL
   }
 };
--- config/pg.js
+++ config/pg.js
@@ -2,5 +2,8 @@ module.exports = {
   development: {
     database: 'contests',
     password: process.env.NODE_PASSWORD || 'password'
+  },
+  production: {
+    connectionString: process.env.NODE_PG_URL
   }
 };
